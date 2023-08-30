1edc18ea62ceb4e69b16a0f17c518664f065b3c6
chichoon <chichoon.choi@gmail.com>
2023-08-31 00:17:46 +0900

    feat:

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,2 @@
 export { ReactComponent as ArrowIcon } from './arrowIcon.svg';
+export { ReactComponent as CommentIcon } from './commentIcon.svg';
--- src/pages/MainPage/IssueDataElement.tsx
+++ src/pages/MainPage/IssueDataElement.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import { IssueType } from 'types/issue';
+import { CommentIcon } from 'assets';
 
 import styles from './IssueDataElement.module.scss';
 
@@ -18,6 +19,10 @@ export const IssueDataElement = ({ issueData }: Props) => (
       <div className={styles.issueInfo}>
         <span>작성자: {issueData.user?.login}</span>
         <span>작성일: {new Date(issueData.created_at).toLocaleDateString()} </span>
+        <div className={styles.issueCommentWrapper}>
+          <CommentIcon />
+          {issueData.comments}
+        </div>
       </div>
     </Link>
   </li>
