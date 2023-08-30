269b21c5d237e7aa1b9358965a222e739004d8bf
chichoon <chichoon.choi@gmail.com>
2023-08-30 20:54:55 +0900

    feat: :sparkles: issue 페이지 컴포넌트 렌더링되도록 등록

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { Layout } from 'components/Layout';
-import { MainPage } from './pages/MainPage';
+import { MainPage } from 'pages/MainPage';
+import { IssuePage } from 'pages/IssuePage';
 
 const App = () => (
   <Routes>
     <Route path='/' element={<Layout />}>
       <Route path='/' element={<MainPage />} />
-      <Route path='/:issueId' element={<div>bbb</div>} />
+      <Route path='/:issueId' element={<IssuePage />} />
     </Route>
   </Routes>
 );
