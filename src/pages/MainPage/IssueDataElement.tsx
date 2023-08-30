cb73728963773306a308da1d60e985f2521cb180
chichoon <chichoon.choi@gmail.com>
2023-08-30 14:45:40 +0900

    feat: :sparkles: 이슈로 넘어가는 링크 추가

--- src/pages/MainPage/IssueDataElement.tsx
+++ src/pages/MainPage/IssueDataElement.tsx
@@ -1,3 +1,5 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 import { IssueType } from 'types/issue';
 
 import styles from './IssueDataElement.module.scss';
@@ -8,13 +10,15 @@ interface Props {
 
 export const IssueDataElement = ({ issueData }: Props) => (
   <li className={styles.issueWrapper}>
-    <div className={styles.issueTitle}>
-      <span>#{issueData.number}</span>
-      <span>{issueData.title}</span>
-    </div>
-    <div className={styles.issueInfo}>
-      <span>작성자: {issueData.user?.login}</span>
-      <span>작성일: {new Date(issueData.created_at).toLocaleDateString()} </span>
-    </div>
+    <Link to={`/${issueData.number}`}>
+      <div className={styles.issueTitle}>
+        <span>#{issueData.number}</span>
+        <span>{issueData.title}</span>
+      </div>
+      <div className={styles.issueInfo}>
+        <span>작성자: {issueData.user?.login}</span>
+        <span>작성일: {new Date(issueData.created_at).toLocaleDateString()} </span>
+      </div>
+    </Link>
   </li>
 );
