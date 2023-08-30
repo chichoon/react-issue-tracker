2e308ae60a0c484d56c67c0e044826f172186c3c
chichoon <chichoon.choi@gmail.com>
2023-08-30 12:41:39 +0900

    feat: :sparkles: 데이터 받아와서 임시 출력

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,37 @@
+import { Octokit } from 'octokit';
+import { useEffect, useState } from 'react';
+
+import { IssueType } from 'types/issue';
+import { getIssues } from 'services/getIssues';
+
 export const MainPage = () => {
-  const octokit = new Octokit();
-  return <div>MainPage</div>;
+  const [dataList, setDataList] = useState<IssueType[]>([]);
+  const [page, setPage] = useState(1);
+  const octokit = new Octokit({
+    auth: process.env.REACT_APP_GITHUB_TOKEN,
+  });
+
+  useEffect(() => {
+    getIssues(page).then((data) => {
+      setDataList(data);
+      console.log(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {dataList.map((data, index) =>
+          index % 5 === 0 && index !== 0 ? (
+            <>
+              <li>광고</li>
+              <li key={data.number}>{data.title}</li>
+            </>
+          ) : (
+            <li key={data.number}>{data.title}</li>
+          )
+        )}
+      </ul>
+    </div>
+  );
 };
